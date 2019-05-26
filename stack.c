b50d1f2beb0fe2f2a2470ba2f6ccd6f579e71396
root <root@DESKTOP-SRTF5C7.localdomain>
2019-05-26 23:06:15 +0700

    first commit

---
+++ stack.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Stack
+{
+    unsigned int capacity;
+    int current;
+    char **array;
+};
+
+struct Stack *createStack(unsigned int capacity);
+void insert(struct Stack *stack, char *data);
+void deleteTop(struct Stack *stack);
+char *checkTop(struct Stack *stack);
+int isEmpty(struct Stack *stack);
+int isFull(struct Stack *stack);
+void deleteStack(struct Stack* stack);
+
+int main(void)
+{
+    struct Stack *stack = createStack(100);
+    insert(stack, "test1");
+    insert(stack, "test2");
+    insert(stack, "test3");
+    insert(stack, "test4");
+    char *data1 = checkTop(stack);
+    printf("result : %s\n", data1);
+    deleteTop(stack);
+    char *data2 = checkTop(stack);
+    printf("result : %s\n", data2);
+    insert(stack, "test5");
+    char *data3 = checkTop(stack);
+    printf("result : %s\n", data3);
+    deleteStack(stack);
+    return 0;
+}
+
+struct Stack *createStack(unsigned int capacity)
+{
+    struct Stack *stack = (struct Stack *)malloc(sizeof(struct Stack));
+    stack->array = (char **)malloc(capacity * sizeof(char *));
+    stack->capacity = capacity;
+    return stack;
+}
+
+void insert(struct Stack *stack, char *data)
+{
+    if (isFull(stack) == 0)
+    {
+        char * currentData = (stack->array)[++(stack->current)] = (char *) malloc(sizeof(char));
+        strcpy(currentData, data);
+    }
+    else
+    {
+        printf("Cannot insert: the stack is full\n");
+    }
+}
+
+void deleteTop(struct Stack *stack)
+{
+    if (isEmpty(stack) == 0)
+    {
+        free((stack->array)[(stack->current)--]);
+    }
+    else
+    {
+        printf("Cannot delete: the stack is empty\n");
+    }
+}
+
+char *checkTop(struct Stack *stack)
+{
+    return (stack->array)[stack->current];
+}
+
+int isEmpty(struct Stack *stack)
+{
+    return (stack->current == 0) ? 1 : 0;
+}
+
+int isFull(struct Stack *stack)
+{
+    return (stack->current == stack->capacity) ? 1 : 0;
+}
+
+void deleteStack(struct Stack* stack) {
+    char** array = stack->array;
+    for(int i = 0; i <= (stack->current); i++) {
+        free(array[i]);
+    }
+    free(stack->array);
+    free(stack);
+}
